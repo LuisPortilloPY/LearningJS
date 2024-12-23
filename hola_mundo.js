be7907723ec9c926d9e0f030d9b80fe3ec7b5f8e
Luis Portillo <portillov.luis@gmail.com>
2024-12-23 11:37:51 -0300

    SecondCommit
    Comentarios

---
+++ hola_mundo.js
@@ -0,0 +1,2 @@
+console.log("Hola Mundo");
+// solo para probar
---
+++ index.js
@@ -0,0 +1,7 @@
+// este es el mensaje de bienvenida
+console.log("esta es la puerta de entrada al proyecto")
+console.log("Adios")
+// tenemos dos formas de comentar.. primeramente el que se usa ahora y siguiente
+/* esta es otra forma de comentar
+en caso de que se quiera usar o se necesiten mas lineas
+para que sea mucho mas entendible */
