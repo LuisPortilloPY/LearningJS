3a1adf5501ba52a799193c7431b4045519d512af
Luis Portillo <portillov.luis@gmail.com>
2025-02-18 21:20:39 -0300

    prettier
    prettier

--- 10-Modulos/detalles.js
+++ 10-Modulos/detalles.js
@@ -1,9 +1,8 @@
-// Formas de importar/exportar modulos 
-// 1. CommonJS  - requiere 
+// Formas de importar/exportar modulos
+// 1. CommonJS  - requiere
 // 2. Import es6 - import
 // const moduloMatematicas= require("./modulos/matematicas")
 // console.log(moduloMatematicas.dividir(10,2))
 // const factorial = moduloMatematicas.factorial(5) de esta forma tambien se puede hacer para llamar uno a uno
 // const  {factorial, sumar, restar, multiplicar, dividir, potencia} = require("./modulos/matematicas") tambien asi y hacerlo por partes. tambien solo lo que necesito.
 // esto es el modo commmonJS
-
--- 10-Modulos/index.js
+++ 10-Modulos/index.js
@@ -1,27 +1,27 @@
-function suma(x,y) {
-    return x+y;
+function suma(x, y) {
+  return x + y;
 }
-function multiplicacion(x,y) {
-    return x*y;
+function multiplicacion(x, y) {
+  return x * y;
 }
-function elevar(x,y) {
-    return x**y;
+function elevar(x, y) {
+  return x ** y;
 }
 function factorial(x) {
-    let fact
-    fact = 1;   
-    for(let i=2; i<=x; i++) {
-        fact *= i;
-    }
-    return fact;
+  let fact;
+  fact = 1;
+  for (let i = 2; i <= x; i++) {
+    fact *= i;
+  }
+  return fact;
 }
 module.exports = {
-    suma,
-    multiplicacion,
-    elevar,
-    factorial
-}
-const fact = factorial(5)
-console.log(fact)
-const sum=suma(5,5)
-console.log(sum)
+  suma,
+  multiplicacion,
+  elevar,
+  factorial,
+};
+const fact = factorial(5);
+console.log(fact);
+const sum = suma(5, 5);
+console.log(sum);
